c0af74e4c48229d8fcde5b15428e22ce4bc619a2
Sandhya-ai-bit <shawsandhya466@gmail.com>
2026-09-14 22:07:00 +0530

    solved an medium level star pattern for logic building

--- Solution2.java
+++ Solution2.java
@@ -281,13 +281,36 @@ class Solution2 {
         }
     }
 
+    public void pattern22(int n) {
+
+        int size = 2 * n - 1;
+
+        for (int i = 0; i < size; i++) {
+
+            for (int j = 0; j < size; j++) {
+
+                int top = i;
+                int left = j;
+                int bottom = size - 1 - i;
+                int right = size - 1 - j;
+
+                int min = Math.min(Math.min(top, bottom),
+                        Math.min(left, right));
+
+                System.out.print((n - min) + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Solution2 s = new Solution2();
 
         int[] arr = { 5 };
 
         for (int n : arr) {
-            s.pattern21(n);
+            s.pattern22(n);
             System.out.println();
         }
     }
