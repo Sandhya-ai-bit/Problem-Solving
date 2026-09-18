5545f5fc3f2fa5cb6a519c1c6eebe13a607db12b
Sandhya-ai-bit <shawsandhya466@gmail.com>
2026-09-18 21:28:11 +0530

    Palindrome Number

---
+++ palindrome.java
@@ -0,0 +1,24 @@
+public class palindrome {
+    public boolean PalindromeNum(int n) {
+        int reverse = 0;
+        int lastdigit;
+        int dup = n;
+        while (n > 0) {
+            lastdigit = n % 10;
+            reverse = (reverse * 10) + lastdigit;
+            n = n / 10;
+        }
+        if (dup == reverse) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        palindrome obj = new palindrome();
+
+        System.out.println(obj.PalindromeNum(77));
+    }
+}
