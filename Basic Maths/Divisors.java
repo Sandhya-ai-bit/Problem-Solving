f7deab752f5101d7df3bcc3c87627cd83e768b11
Sandhya-ai-bit <shawsandhya466@gmail.com>
2026-09-19 12:31:42 +0530

    Divisors of a Number

--- Basic Maths/Divisors.java
+++ Basic Maths/Divisors.java
@@ -1,15 +1,31 @@
+import java.util.Arrays;
+
 public class Divisors {
-    public void divisors(int n) {
+    public int[] divisors(int n) {
+        int count = 0;
+        // count divisors
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
-                System.out.println(i);
+                count++;
             }
         }
-
+        // create array
+        int[] result = new int[count];
+        int index = 0;
+        // store the divisors
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                result[index] = i;
+                index++;
+            }
+        }
+        // return array
+        return result;
     }
 
     public static void main(String[] args) {
         Divisors obj = new Divisors();
-        obj.divisors(36  );
+        int[] answer = obj.divisors(63);
+        System.out.println(Arrays.toString(answer));
     }
 }
